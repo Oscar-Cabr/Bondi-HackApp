19e0da0774ccfc59faea8c8a14f48e8f26e4a752
SegFaultPapi <0xandres.rmdo@gmail.com>
2026-04-20 12:41:11 -0600

    Initial Commit

---
+++ Bondi/BondiApp.swift
@@ -0,0 +1,17 @@
+//
+//  BondiApp.swift
+//  Bondi
+//
+//  Created by Andrés Rodríguez Montes de Oca on 20/04/26.
+//
+
+import SwiftUI
+
+@main
+struct BondiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
